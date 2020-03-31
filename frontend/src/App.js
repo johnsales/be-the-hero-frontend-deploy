8d2aaaad3d33d569f09c9266d435f48e12516fd3
John Santiago <jmsvsmorone@hotmail.com>
2020-03-30 21:49:40 -0300

    first

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Routes from './routes';
+
+import './global.css';
+
+function App() {
+
+  return (
+    //JSX html integrado dentro do java script
+    //<h1>Hello World</h1>
+    <Routes />
+  );
+}
+
+export default App;
